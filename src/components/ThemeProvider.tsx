2649c1cd1e0714be1e824cba1a99aff6c1257cf9
y93669 <114243660+y93669@users.noreply.github.com>
2025-08-30 15:39:19 +0800

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     <NextThemesProvider
       attribute='class'
       defaultTheme='dark'
-      enableSystem
+      enableSystem={false}
       {...props}
     >
       {children}
